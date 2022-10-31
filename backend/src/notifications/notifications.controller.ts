813e1ced8ea0aaf28191dd75f30feca0889f2a3d
AiramAire <levantateyanda.aire@gmail.com>
2022-10-31 14:26:55 +0100

    New implementation

--- backend/src/notifications/notifications.controller.ts
+++ backend/src/notifications/notifications.controller.ts
@@ -1,42 +1,16 @@
-import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
 import { Body, Controller, Post } from '@nestjs/common';
-import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
-import { CreatedNotificationDto, ReceivedNotificationDto } from './dto/notification.dto';
+import { ReceivedNotificationDto } from './dto/notification.dto';
 import { NotificationsService } from './notifications.service';
 
 export class ReceivedDataNotification {
   body: ReceivedNotificationDto[];
 }
-
-export class ReceviedDataIds {
-  body: string[];
-}
-
 @Controller('notifications')
 export class NotificationsController {
-  private client: ClientProxy;
-
-  constructor(
-    @InjectRedis() private readonly redis: Redis,
-    private readonly notificationsService: NotificationsService
-  ) {
-    this.client = ClientProxyFactory.create({
-      transport: Transport.REDIS,
-      options: {
-        url: 'redis://localhost:6360',
-      },
-    });
-  }
+  constructor(private readonly notificationsService: NotificationsService) {}
 
   @Post('addNotifications')
-  async addNotifications(
-    @Body() data: ReceivedDataNotification
-  ): Promise<CreatedNotificationDto[]> {
-    return this.notificationsService.addNotifications(data.body);
-  }
-
-  @Post('update')
-  async update(@Body() data: ReceviedDataIds): Promise<void> {
-    await this.notificationsService.update(data.body);
+  async addNotifications(@Body() data: ReceivedDataNotification): Promise<void> {
+    this.notificationsService.addNotifications(data.body);
   }
 }
--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,9 +1,29 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { CreateConsumer, DoneConsumer } from './consumer-producer';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
 @Module({
+  imports: [
+    BullModule.registerQueue(
+      {
+        name: 'create-notifications-queue',
+        redis: {
+          host: 'localhost',
+          port: 6360,
+        },
+      },
+      {
+        name: 'done-notifications-queue',
+        redis: {
+          host: 'localhost',
+          port: 6360,
+        },
+      }
+    ),
+  ],
   controllers: [NotificationsController],
-  providers: [NotificationsService]
+  providers: [NotificationsService, NotificationsService, CreateConsumer, DoneConsumer],
 })
 export class NotificationsModule {}
